6f35e1f2e56c278e08c320d52789f1ed2b176de5
PyroGamez <pushkal.kafley@gmail.com>
2021-03-25 15:31:26 -0400

    2ND commit pog

--- sateliteImages.py
+++ sateliteImages.py
@@ -1,23 +1,24 @@
 import requests
-import ctypes
 from geopy.geocoders import Nominatim
 
 
+apiKey = "DEMO_KEY" #ENTER YOUR API KEY HERE | this one will work, but you'll get limited after some pulls
 geolocator = Nominatim(user_agent="smolApp")
 
 inputAddr = input("Address: ")
-
+#loacting your addrs lat and lon
 location = geolocator.geocode(inputAddr)
 print(location.address)
 lat = location.latitude 
 lon = location.longitude
 
-
-link =f"https://api.nasa.gov/planetary/earth/imagery?lon={lon}&lat=lat{lat}&api_key=9aUenJfd9glnuABx7O9iYIrNBl7FSUz6Ns3BrTBS"
+#formatted link w/ your info
+link =f"https://api.nasa.gov/planetary/earth/imagery?lon={lon}&lat=lat{lat}&api_key={apiKey}"
 out = requests.get(link)
 
 imgName = f"sateliteImg.jpg"
 
+#saving the img of the satelite img
 with open(imgName, "wb") as f:
     f.write(requests.get(link).content)
 
